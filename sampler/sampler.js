53598d8b71b8bed7dd00b688e67e3b24672880e9
chwoods1 <132087868+chwoods1@users.noreply.github.com>
2026-03-15 14:56:52 -0400

    Added basic node.js docker configuration

---
+++ sampler/sampler.js
@@ -0,0 +1,11 @@
+let http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
+
+async function sample_data() {
+    const sensordata = await fetch("chase-sensor-1/read");
+    print(sensordata);
+}
