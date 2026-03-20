1b661f48e4b9118780da999b70d8f58e9fb212a3
Tommy McQuiston <145158264+tlmcquis@users.noreply.github.com>
2026-03-19 22:03:02 -0400

    Update transformer.py

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,1 +1,23 @@
+import os, time, math, requests 
+from flask import Flask, jsonify, request
+from app import app
 
+@app.route('/transform', methods=['POST'])
+def transform():
+    #gets the voltage and timestamp from POST
+    if request.method == 'POST':
+        voltage = request.form.get('voltage')
+        timestamp = request.form.get('timestamp')
+        
+    # Check if voltage is provided
+    if voltage is None:
+        return jsonify({'error': 'Voltage value is required'}), 400
+    temperature = transform_data(voltage)
+    return jsonify({'temperature': temperature, 'timestamp': timestamp}), 200
+    
+
+def transform_data(voltage):
+    # Converts the voltage to a temperature
+    temperature = (voltage - 0.5) * 100
+    print(f"Temperature: {round(temperature, 2)} °C")
+    return round(temperature, 2)
